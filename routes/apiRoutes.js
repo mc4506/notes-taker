bcc2c4a7d734e06894e4747f001812331dba569d
Mike S Chen <mike.sihong.chen@gmail.com>
2020-08-07 22:53:01 -0400

    added DELETE api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,24 +7,21 @@ const fs = require("fs");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+let idCounter = 0;
+
 routes.get("/api/notes", function(req, res){
     res.sendFile(path.join(__dirname, "../db/", "db.json"));
 })
 
-let notes=[];
-
 routes.post("/api/notes", function(req, res){
+    let notes=[];
     readFileAsync("./db/db.json", "utf8").then( data => {
         notes = JSON.parse(data);
         // console.log(notes);
     }).then(() => {
         // console.log(req.body);
         notes.push(req.body);
-        notes.forEach((e, i) => {
-            if(!e.id) {
-                e["id"] = i;
-            }
-        });
+        notes.forEach((e, i) => e["id"] = i);
         console.log(notes);
         writeFileAsync("./db/db.json", JSON.stringify(notes));
      }).catch(err => {
@@ -33,7 +30,22 @@ routes.post("/api/notes", function(req, res){
 })
 
 routes.delete("/api/notes/:id", function(req, res){
-
+    const chosen = req.params.id;
+    console.log(`delete ${chosen}`);
+    let notes=[];
+    readFileAsync("./db/db.json", "utf8").then( data => {
+        notes = JSON.parse(data);
+    }).then(() => {
+        notes.forEach((e, i) => {
+            if(e.id === parseInt(chosen)){
+                notes.splice(i, 1);
+            }
+        })
+        console.log(notes);
+        writeFileAsync("./db/db.json", JSON.stringify(notes));
+    }).catch(err => {
+        if(err) throw err;
+    })
 })
 
 
