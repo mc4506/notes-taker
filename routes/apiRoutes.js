905afcf92d9eca14a88af02b9b7bc47d87be6177
Mike S Chen <mike.sihong.chen@gmail.com>
2020-08-07 20:55:57 -0400

    added POST route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const routes = express.Router();
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
 
-routes.get("/api", function(req, res){
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+routes.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "../db/", "db.json"));
+})
+
+let notes=[];
+
+routes.post("/api/notes", function(req, res){
+    readFileAsync("./db/db.json", "utf8").then( data => {
+        notes = JSON.parse(data);
+        // console.log(notes);
+    }).then(() => {
+        // console.log(req.body);
+        notes.push(req.body);
+        console.log(notes);
+        writeFileAsync("./db/db.json", JSON.stringify(notes));
+     }).catch(err => {
+         if(err) throw err;
+     })
+})
+
+routes.delete("/api/notes/:id", function(req, res){
 
 })
+
 
 module.exports = routes;
