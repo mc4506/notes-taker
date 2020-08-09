34672bab1e3c5ecf16292563a3456456d4417d80
Mike S Chen <mike.sihong.chen@gmail.com>
2020-08-08 20:14:18 -0400

    added tests for routes

--- test/api.test.js
+++ test/api.test.js
@@ -1,49 +1,55 @@
 const express = require("express");
 const app = express();
-// const apiRoutes = require("./routes/apiRoutes.js");
 const supertest = require("supertest");
 const request = supertest(app);
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 
 app.get("/test", (req, res) => {
-    res.sendFile(path.join(__dirname, "./", "test.json"));
+    res.json('{"title":"test"}');
 })
 
-// app.post("/test", (req, res) => {
-//     fs.readFile("./test/test.json", function(err, data) {
-//         if (err) throw err;
-//         console.log(body);
-//     })
-//     res.json(req.body);
-// })
+app.post("/post", (req, res) => {
+    let testArr = [];
+    testArr.push(req.body);
+    res.json(testArr);
+})
 
+app.delete("/delete/:id", (req, res) => {
+    let testArr = [{"title":"delete", "id":"0"}];
+    let chosen = req.params.id;
+    if(testArr[0].id === chosen){
+        testArr.splice(0,1);
+    }
+    res.json(testArr);
+})
 
-describe("GET", function(){
-    it("should get the json object in test.json", async function(done){
+describe("GET", function () {
+    it("should get the json object in the response", async (done) => {
         const response = await request.get("/test");
         expect(response.status).toBe(200);
-        // console.log(response);
-        expect(response.body).toStrictEqual(JSON.parse('[{"title":"Test Title","text":"Test text"}]'));
+        expect(response.body).toStrictEqual(('{"title":"test"}'));
         done();
     })
 })
 
-// describe("POST", function(){
-//     it("should append to the array in /test", async function(done){
-//         const testObj = {
-//             "title": "Post Test",
-//             "text": "testing"
-//         }
-//         const testArr = [];
+describe("POST", function () {
+    it("should POST testObj and receive the testObj back in the response", async function (done) {
+        const testObj = {
+            "title": "Post",
+        };
+        const response = await request.post("/post").send(testObj);
+        expect(response.text).toBe('[{"title":"Post"}]');
+        done();
+    })
+})
 
-//         const response = await request.post("/test/");
-//         // console.log(response)
-//         // testArr.push(response);
-
-//         expect(response.status).toBe(201);
-//         // expect(response.body).toHaveProperty("post");
-//         done();
-//     })
-// })
+describe("DELETE", function () {
+    it("should DELETE obj in the test array and get an empty array back in the response", async function (done) {
+        const response = await request.delete("/delete/0");
+        expect(response.text).toBe('[]');
+        done();
+    })
+})
