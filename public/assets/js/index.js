e370dc7900ff4578faa9fe42fb2af3dbe59ea3bf
Mike S Chen <mike.sihong.chen@gmail.com>
2020-08-09 09:36:57 -0400

    added responsive modal class and styles for xs screens

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -133,7 +133,7 @@ const renderNoteList = (notes) => {
   // Returns jquery object for li with given text and delete button
   // unless withDeleteButton argument is provided as false
   const create$li = (text, withDeleteButton = true) => {
-    const $li = $("<li class='list-group-item'>");
+    const $li = $("<li class='list-group-item' data-dismiss='modal'>");
     const $span = $("<span>").text(text);
     $li.append($span);
 
